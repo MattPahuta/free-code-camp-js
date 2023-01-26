8cd4a8411bd790c3fccc54a61ca569aa1854d271
Matt Pahuta <mhpahuta@gmail.com>
2023-01-26 10:33:00 -0800

    Add Jan 26 2023 progress

--- algorithm-scripting.js
+++ algorithm-scripting.js
@@ -7,8 +7,37 @@
 
 // *** Seek and Destroy 
 /*
+You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
 
+Note: You have to use the arguments object.
+
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) => [1,5,1]
+destroyer(["tree", "hamburger", 53], "tree", 53) => ["hamburger"].
 */
+
+function destroyer(arr) {
+
+  // using Array.from, arguments, slice, filter, and includes
+  const valsToRemove = Array.from(arguments).slice(1)
+  return arr.filter(el => !valsToRemove.includes(el));
+
+  // With Object.values and nested for loops, conditional
+  // const valsToRemove = Object.values(arguments).slice(1);
+  // const filteredArray = [];
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   let removeElement = false;
+  //   for (let j = 0; j < valsToRemove.length; j++) {
+  //     if (arr[i] === valsToRemove[j]) {
+  //       removeElement = true;
+  //     }
+  //   }
+  //   if (!removeElement) {
+  //     filteredArray.push(arr[i]);
+  //   }
+  // }
+  // return filteredArray;
+}
 
 
 
