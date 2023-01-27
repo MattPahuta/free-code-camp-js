8ed882c584f93e299d74a2504b69691aa09508f2
Matt Pahuta <mhpahuta@gmail.com>
2023-01-27 13:22:10 -0800

    Jan 27 2023 progress

--- algorithm-scripting.js
+++ algorithm-scripting.js
@@ -5,6 +5,63 @@
 // *** Intermediate Algorithm Scripting *** //
 // **************************************** //
 
+// *** Wherefore art thou
+// *** (Find matching Objects in an array of objects)
+/*
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [
+  { first: "Romeo", last: "Montague" }, 
+  { first: "Mercutio", last: null }, 
+  { first: "Tybalt", last: "Capulet" }
+  ], 
+  and the second argument is { last: "Capulet" }, 
+  
+  then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+
+*/
+function whatIsInAName(collection, source) {
+  // take in a collection - an array of objects
+  // take in an object of key/value pairs (source)
+  // loop over the collection and see if any of the objects contain the all of the key/value pairs of the source object
+  // return an array of the matching collection objects 
+  // return an empty object if no matches 
+
+  // with a for loop, for...in loop, push
+  const result = [];
+  for (let i = 0; i < collection.length; i++) {
+    let found = true;
+    for (const sourceProp in source) {
+      if (collection[i][sourceProp] !== source[sourceProp]) {
+        found = false;
+        break;
+      }
+    }
+    if (found) result.push(collection[i])
+  }
+  return result;
+
+  const sourceKeys = Object.keys(source);
+
+  // with filter, every, and hasOwnProperty
+  return collection.filter(obj => {
+    return sourceKeys.every(key => obj.hasOwnProperty(key) && obj[key] === source[key])
+  })
+
+
+  // with filter, for loop, conditional, and hasOwnProperty
+  // return collection.filter(obj => {
+  //   for (let i = 0; i < sourceKeys.length; i++) {
+  //     if (!obj.hasOwnProperty(sourceKeys[i]) || obj[sourceKeys[i]] !== source[sourceKeys[i]]) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // })
+
+}
+
+
 // *** Seek and Destroy 
 /*
 You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
