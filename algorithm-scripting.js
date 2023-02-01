92bd5f93500140eb434a48c59da19eecee58354a
Matt Pahuta <mhpahuta@gmail.com>
2023-02-01 13:02:32 -0800

    Feb 1 2023 progress

--- algorithm-scripting.js
+++ algorithm-scripting.js
@@ -6,27 +6,59 @@
 // **************************************** //
 
 
-function sum(arr, n) {
-  if (n <= 0) {
-    return 0;
+// *** Search and Replace 
+/*
+Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+- First argument is the sentence to perform the search and replace on.
+- Second argument is the word that you will be replacing (before).
+- Third argument is what you will be replacing the second argument with (after).
+
+Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
+
+*/
+function myReplace(str, before, after) {
+
+  // find the index of 'before' in 'str'
+  const index = str.indexOf(before);
+  // check if first letter is uppercase or not
+  if (str[index] === str[index].toUpperCase()) {
+    // change the after word to be capitalized before using it:
+    after = after.charAt(0).toUpperCase() + after.slice(1);
   } else {
-    return sum(arr, n - 1) + arr[n - 1];
+    // change the after word to uncapitalized before using it:
+    after = after.charAt(0).toLowerCase() + after.slice(1);
   }
+  // now do the replace on the original str
+  return str.replace(before, after)
+
 }
+
 
 // *** Pig Latin 
 /*
-
-
 Pig Latin is a way of altering English Words. The rules are as follows:
 - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
 - If a word begins with a vowel, just add way at the end.
-
-Translate string to Pig Latin. Input words are English and all in lowercase
-
+Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
 */
+
 function translatePigLatin(str) {
-  
+
+  // solution 2
+  if (str.match(/^[aeiou]/)) return str + "way";
+
+  const consonantCluster = str.match(/^[^aeiou]+/)[0];
+  return str.substring(consonantCluster.length) + consonantCluster + "ay";
+
+  // solution 1
+  let consonantRegex = /^[^aeiou]+/;
+  let myConsonants = str.match(consonantRegex);
+  return myConsonants !== null
+    ? str
+        .replace(consonantRegex, "")
+        .concat(myConsonants)
+        .concat("ay")
+    : str.concat("way");
 }
 
 
