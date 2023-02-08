11f2ab9a5e6722f9f7939917a71cd0d3abc84322
Matt Pahuta <mhpahuta@gmail.com>
2023-02-07 19:43:50 -0800

    Feb 7 2023 progress

--- algorithm-scripting.js
+++ algorithm-scripting.js
@@ -5,6 +5,56 @@
 // *** Intermediate Algorithm Scripting *** //
 // **************************************** //
 
+function factorializeIt(n) {
+  return n <= 1 ? 1 : n * factorialize(n - 1);
+}
+
+
+// *** DNA Pairing 
+/*
+Pairs of DNA strands consist of nucleobase pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.
+
+The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+
+Examples:
+pairElement("ATCGA") => [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+Waiting: pairElement("TTGAG") => [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+Waiting: pairElement("CTCTA") => [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
+*/
+function pairElement(str) {
+  // with split, map, and a switch statement
+  // return str.split('').map(char => {
+  //   switch (char) {
+  //     case 'G':
+  //       return ['G', 'C']
+  //       break;
+  //     case 'C':
+  //       return ['C', 'G']
+  //       break;
+  //     case 'A':
+  //       return ['A', 'T']
+  //       break;
+  //     case 'T':
+  //       return ['T', 'A']
+  //       break;
+  //   }
+  // })
+
+  // using a pairs object as a lookup, with split and map
+  const pairs = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C"
+  };
+
+  return str.split('').map(char => [char, pairs[char]])
+
+}
 
 // *** Search and Replace 
 /*
