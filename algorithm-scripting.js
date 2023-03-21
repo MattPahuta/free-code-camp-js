617dde5f1b8643516d48ec8cd933e831e7d0c6d3
Matt Pahuta <mhpahuta@gmail.com>
2023-03-21 16:35:35 -0700

    Mar 21 2023 progress

--- algorithm-scripting.js
+++ algorithm-scripting.js
@@ -6,6 +6,42 @@
 // **************************************** //
 
 
+// *** Sum All Primes
+/*
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+*/
+function sumPrimes(num) {
+  // get a list of all prime numbers <= num
+  // sum the prime numbers
+
+  // *** use a for loop, every, and reduce
+  let primes = [];
+  for (let i = 2; i <= num; i++) {
+    if (primes.every((prime) => i % prime !== 0))
+      primes.push(i);
+  }
+  return primes.reduce((sum, prime) => sum + prime, 0);
+
+
+  // *** use a helper function with a for loop
+  function isPrime(num) {
+    if (num < 2) return false;
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) sum += i;
+  }
+  return sum;
+
+}
+
 // *** Sum all Odd Fibonacci Numbers 
 /*
 Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
